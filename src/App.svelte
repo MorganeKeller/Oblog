07d5a70d3b3730b6d44516a81c358fd884a38652
MorganeKeller <Nogmera2701@gmail.com>
2023-04-12 15:59:11 +0200

    CGU & ArticlesList pages HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script>
   import Header from "./lib/Header.svelte";
   import Footer from "./lib/Footer.svelte";
-
+  import Articles from "./lib/ArticlesList.svelte";
+  import CGU from "./lib/CGU.svelte";
   import Welcome from "./lib/Welcome.svelte";
 </script>
 
 <main>
   <Header />
 
-  <Welcome />
+  <CGU />
 
   <Footer />
 </main>
--- src/lib/ArticlesList.svelte
+++ src/lib/ArticlesList.svelte
@@ -1,1 +1,23 @@
 <script></script>
+
+<main>
+    <h1>Tous les articles</h1>
+    <p>Ici tu peux consulter tous nos articles</p>
+    <div>
+        <article>
+            <h2>Article 1</h2>
+            <p>Contenu de l'article</p>
+            <a href="">Lire la suite</a>
+        </article>
+        <article>
+            <h2>Article 2</h2>
+            <p>Contenu de l'article</p>
+            <a href="">Lire la suite</a>
+        </article>
+        <article>
+            <h2>Article 3</h2>
+            <p>Contenu de l'article</p>
+            <a href="">Lire la suite</a>
+        </article>
+    </div>
+</main>
