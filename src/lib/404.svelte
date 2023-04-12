dcf0f36ff672748da0ea7dc06dde89fce47b5f1a
MorganeKeller <Nogmera2701@gmail.com>
2023-04-12 15:05:49 +0200

    save

--- src/lib/404.svelte
+++ src/lib/404.svelte
@@ -4,7 +4,7 @@
     <div>
         <h1>Oups!</h1>
         <p>Il semblerait que tu te sois perdu ou que la page recherchée n'ait pas été trouvée...</p>
-        <p>Pour retourner à l'accueil c'est <a href="#" aria-label="retour vers l'accueil">par ici</a></p>
+        <p>Pour retourner à l'accueil c'est <a href="" aria-label="retour vers l'accueil">par ici</a></p>
     </div>
 
 </main>
