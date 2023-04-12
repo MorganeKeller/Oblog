315303abb968b0da953ccaf23a6adcb4cd00cbae
QuentinPicot <quentinpicot50@icloud.com>
2023-04-12 15:55:33 +0200

    Page Contact

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,16 @@
 <script>
   import Header from "./lib/Header.svelte";
   import Footer from "./lib/Footer.svelte";
+  
 
   import Welcome from "./lib/Welcome.svelte";
+    import Contact from "./lib/Contact.svelte";
 </script>
 
 <main>
   <Header />
 
-  <Welcome />
+  <Contact />
 
   <Footer />
 </main>
--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -4,12 +4,14 @@
     <h1>Formulaire de contact</h1>
     <div>
         <p>
-            Voici notre page de contact! Si tu aperçois une coquille, une
-            erreur, un propos un peu déplacé avant qu'on ait pu mettre la main
-            dessus, fais-le nous savoir ici. Ou alors, si tu veux juste nous
-            envoyer un petit message pour nous dire ô combien on est chouette,
-            ça marche aussi!
+            Bonjour et bienvenue sur mon blog de voyage ! Si vous cherchez à nous contacter, n'hésitez pas à m'envoyer yb mail en utilisant le formulaire de contact disponible sur mon site.
+            Nous sommes toujours heureux de recevoir des commentaires, des questions ou des suggestions de la part de nos lecteurs. nous ferons de notre mieux pour vous y répondre dans les meilleurs
+            délais. Merci de votre intérêt pour notre blog et nous espèrons avoir de vos nouvelles bientôt !
         </p>
+
+        <input type="search" name="" placeholder="Name">
+        <input type="search" name="" placeholder="Email">
+        <input type="search" name="" placeholder="Message">
     </div>
     
 </main>
