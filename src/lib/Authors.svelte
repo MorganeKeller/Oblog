fa976f5ad65de90ee4b87cac39dec8cc746b6d19
charaah <firdaws.kurayshy@gmail.com>
2023-04-13 13:39:55 +0200

    save

--- src/lib/Authors.svelte
+++ src/lib/Authors.svelte
@@ -1,14 +1,26 @@
 <script></script>
 
-<head>
-	<title>Liste des auteurs</title>
-</head>
+
 <body>
 	<h1>Liste des auteurs</h1>
-	<ul>
-		<li>Chahra Pova</li>
-		<li>Morgan Freeman</li>
-		<li>Olivier Giroud</li>
-		<li>Quentin Tarentino</li>
+
+    <h2>Morgan Freeman</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+       Ipsa, eum? Explicabo, voluptatem adipisci. </p>
+       <img src="" alt="">
+    <h2>Chahra Pova</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+       Ipsa, eum? Explicabo, voluptatem adipisci. </p>
+       <img src="" alt="">
+    <h2>Quentin Tarentino</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+       Ipsa, eum? Explicabo, voluptatem adipisci. </p>
+       <img src="" alt="">
+    <h2>Olivier Giroud</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+       Ipsa, eum? Explicabo, voluptatem adipisci. </p>
+       <img src="" alt="">
+
+	
 		
 </body>
