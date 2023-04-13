eff96ab6ee72e65c991204ca723f769565b42d89
QuentinPicot <quentinpicot50@icloud.com>
2023-04-13 13:34:31 +0200

    add

--- src/App.svelte
+++ src/App.svelte
@@ -5,28 +5,20 @@
   
 
   import Welcome from "./lib/Welcome.svelte";
-<<<<<<< HEAD
   import Contact from "./lib/Contact.svelte";
 
   
 
 
-=======
->>>>>>> 46221ac8204ce27820fcac6bbffdb8a31a370042
 
 </script>
 
 <main>
   <Header />
 
-<<<<<<< HEAD
   <Contact/>
   
   
-=======
-
-
->>>>>>> 46221ac8204ce27820fcac6bbffdb8a31a370042
 
   <Footer />
 </main>
