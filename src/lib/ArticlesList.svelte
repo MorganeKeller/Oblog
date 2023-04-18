54d94fe6dec67ae0a6d5edf19f82a3c24db1ddca
OlivierYA <yoh009@msn.com>
2023-04-18 17:56:42 +0200

    article list ultra wide V1

--- src/lib/ArticlesList.svelte
+++ src/lib/ArticlesList.svelte
@@ -6,33 +6,43 @@
 </script>
 
 <section class="aurore-boreale_background">
+
     <div class="content-center">
+
         <h1>Vos expériences</h1>
 
         <h2>Ecris nous tes aventures</h2>
 
-            <p>C'est ici que nous partageons nos expériences</p>
+        <p>C'est ici que nous partageons nos expériences</p>
 
-        <a  href="">A ta plume</a>
+        <a href="">A ta plume</a>
 
         <article>
             <h3>Le Japon</h3>
-            <img src={tokyo} alt="" />
-            <p><strong>OY</strong></p>
-            <time datetime="2011-11-18T14:54:39">18 Nov 11</time>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-                officiis totam, ab a sed labore voluptatem natus corporis
-                commodi voluptates consequuntur aspernatur maiores asperiores
-                explicabo unde. Quos eveniet maxime voluptates! Excepturi
-                dignissimos similique fuga. Id praesentium iusto in debitis
-                tenetur dolorem nesciunt maiores aliquid! Enim?
-            </p>
-            <a use:link href="/article">Lire la suite</a>
+
+            <div class="alignement">
+                <img src={tokyo} alt="" />
+                
+                    <p><strong>OY</strong></p> 
+                    <time datetime="2011-11-18T14:54:39">18 Nov 11</time>
+             
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Alias officiis totam, ab a sed labore voluptatem natus
+                    corporis commodi voluptates consequuntur aspernatur maiores
+                    asperiores explicabo unde. Quos eveniet maxime voluptates!
+                    Excepturi dignissimos similique fuga. Id praesentium iusto
+                    in debitis tenetur dolorem nesciunt maiores aliquid! Enim?
+                </p>
+                <a use:link href="/article">Lire la suite</a>
+            </div>
         </article>
 
         <article>
             <h3>La Nouvelle-Zélande</h3>
+
+            <div class="alignement">
             <img src={new_zealand2} alt="" />
             <p><strong>MK</strong></p>
             <time datetime="2011-11-18T14:54:39">18 Nov 11</time>
@@ -45,10 +55,14 @@
                 magni iure soluta sint adipisci.
             </p>
             <a href="">Lire la suite</a>
+
+            </div>
         </article>
 
         <article>
             <h3>Les Etats-Unis</h3>
+
+            <div class="alignement">
             <img src={grand_canyon} alt="" />
             <p><strong>QP</strong></p>
             <time datetime="2011-11-18T14:54:39">18 Nov 11</time>
@@ -63,6 +77,8 @@
                 reiciendis aliquam ut doloremque.
             </p>
             <a href="">Lire la suite</a>
+
+            </div>
         </article>
     </div>
 </section>
