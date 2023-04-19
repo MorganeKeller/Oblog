253137cbea721c11c10c0e3a012b518775fd1169
charaah <firdaws.kurayshy@gmail.com>
2023-04-19 16:24:07 +0200

    contact v1

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -1,24 +1,29 @@
-<script></script>
+<script>
+  import contact from "../assets/scss/contact.scss";
 
+</script>
 
-    <h1>Formulaire de contact</h1>
-    <div>
-        <p>
-            Bonjour et bienvenue sur mon blog de voyage ! Si vous cherchez à nous contacter, n'hésitez pas à m'envoyer yb mail en utilisant le formulaire de contact disponible sur mon site.
-            Nous sommes toujours heureux de recevoir des commentaires, des questions ou des suggestions de la part de nos lecteurs. nous ferons de notre mieux pour vous y répondre dans les meilleurs
-            délais. Merci de votre intérêt pour notre blog et nous espèrons avoir de vos nouvelles bientôt !
-        </p>
-
-        
-<form>
-        
-
-        <textarea  name="" placeholder="Name"></textarea>
-        <textarea  name="" placeholder="Email"></textarea>
-        <textarea  name="" placeholder="Message"></textarea>
-</form>
-       
-</div>
-   
-
-    
+<section>
+    <div class="container">
+      <form>
+        <label>
+          Nom :
+          <input type="text" name="name" required>
+        </label>
+  
+        <label>
+          Email :
+          <input type="email" name="email" required>
+        </label>
+  
+        <label>
+          Message :
+          <textarea name="message" required></textarea>
+        </label>
+  
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+</section>
+  
+  
