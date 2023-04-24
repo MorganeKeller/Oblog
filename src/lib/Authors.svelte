81c583f5d669127c145997cbfa75247bd41aff40
OlivierYA <yoh009@msn.com>
2023-04-24 17:01:02 +0200

    author V1

--- src/lib/Authors.svelte
+++ src/lib/Authors.svelte
@@ -1,26 +1,43 @@
 <script>
    import author from '../assets/scss/author.scss';
+   import { link } from "svelte-spa-router";
+
+
+   const getAuthors = async function () {
+      const endpoint = import.meta.env.VITE_URL_DIRECTUS + 'items/user/';
+      const response = await fetch (endpoint);
+
+      const json= await response.json ();
+
+      return json.data;
+   }
+   console.log(getAuthors);
+
 
 
 </script>
 
 <section class="authors">
+
+   {#await getAuthors()}
+
+   <p>chargement en cours...</p>
+      
+   {:then utilisateurs} 
+      
+   
    <h1>Les Auteurs</h1>
+   {#each utilisateurs as auteur}
+      
+   
+   <div class="one-author">
+      <h2>{auteur.user_name}</h2>
 
-   <div class="one-author">
-      <h2>Morgan Freeman</h2>
+      <a use:link href="/articlesList">Liste des articles</a>
       
    </div>
-   <div class="one-author">
-      <h2>Chahra Pova</h2>
-      
-   </div>
-   <div class="one-author">
-      <h2>Quentin Tarentino</h2>
-      
-   </div>
-   <div class="one-author">
-      <h2>Olivier Giroud</h2>
+   
 
-   </div>
+   {/each}
+   {/await}
 </section>
