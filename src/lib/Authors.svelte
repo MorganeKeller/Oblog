3e598a0cf5e176742d66d7e1abbe141004457985
OlivierYA <yoh009@msn.com>
2023-04-24 14:39:48 +0200

    supression description author

--- src/lib/Authors.svelte
+++ src/lib/Authors.svelte
@@ -9,27 +9,18 @@
 
    <div class="one-author">
       <h2>Morgan Freeman</h2>
-      <p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea minima natus at eveniet molestiae amet recusandae repellendus molestias aperiam vero cupiditate dolores ullam, rerum animi. Velit iste, commodi amet possimus minima voluptas voluptate sit harum fugiat impedit nostrum similique suscipit nemo magni sequi doloribus illum, ab eaque nesciunt? Nobis amet minima ipsum totam velit exercitationem nihil, accusantium recusandae! Dicta illum fugiat optio! Hic dolores quaerat, amet aperiam praesentium, ipsam facilis quod excepturi quam delectus blanditiis perspiciatis alias error itaque consequatur.
-      </p>
+      
    </div>
    <div class="one-author">
       <h2>Chahra Pova</h2>
-      <p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repudiandae dicta fugit praesentium magnam corporis quos dolorem natus repellat ducimus debitis iusto, repellendus aliquid nemo labore asperiores ad rerum, culpa eum. Necessitatibus quisquam eius debitis.  Iste voluptatum consectetur eaque recusandae deleniti quis suscipit et laborum. Illo id reprehenderit aspernatur est quidem eveniet quaerat, quia vel. 
-      </p>
+      
    </div>
    <div class="one-author">
       <h2>Quentin Tarentino</h2>
-      <p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus architecto nobis rerum animi placeat quaerat, expedita quam ex, voluptatibus numquam sint omnis aspernatur molestias id doloribus vitae velit cupiditate? Cupiditate exercitationem minima itaque atque dolore repellat voluptate sit tempore libero. Illum consequuntur esse deserunt dicta ipsum dignissimos expedita doloribus, dolor repellat labore mollitia minima quia nisi nihil at voluptate modi distinctio earum corrupti necessitatibus. Quam quae aut, illum porro deleniti laudantium. Quis odio sit delectus laborum deleniti aut qui exercitationem doloremque a. Illum id maiores, ea exercitationem ex quo provident quas atque voluptate quasi blanditiis, veniam sed repudiandae perferendis similique.
-      </p>
+      
    </div>
    <div class="one-author">
       <h2>Olivier Giroud</h2>
-      <p>
-         Eius ex doloremque adipisci aliquam suscipit a velit odit, ad distinctio vitae ducimus ab repudiandae cumque dicta temporibus similique dolore vel doloribus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, eum?
-         Explicabo, voluptatem adipisci.
-      </p>
+
    </div>
 </section>
