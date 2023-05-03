eccc4adfe8861b34d5ee169daa318b66db2482bb
charaah <firdaws.kurayshy@gmail.com>
2023-05-03 16:29:57 +0200

    categoryarticles

--- src/lib/CategoryArticles.svelte
+++ src/lib/CategoryArticles.svelte
@@ -2,59 +2,49 @@
   import { link } from "svelte-spa-router";
   import { onMount } from 'svelte';
 
-  export let categoryId;
+  let articles = [];
 
-  const continents = ["Europe", "Asie", "Afrique", "Amérique du Nord", "Amérique du Sud", "Océanie"];
-  let articlesByContinent = {};
+  export let params = {};
 
-  const getArticlesByContinent = async (continent) => {
+
+  const getArticlesByContinent = async () => {
     const endpoint =
       import.meta.env.VITE_URL_DIRECTUS +
-      `items/article?fields=*&filter[category_id][_eq]=${categoryId}&filter[continent][_eq]=${continent}&sort=-created_at`;
+      `items/article?fields=*&filter[category_id][_eq]=${params.categoryId}&sort=-created_at`;
     const response = await fetch(endpoint);
     const json = await response.json();
-    articlesByContinent[continent] = json.data.map((article) => {
+    articles = json.data.map((article) => {
       return {
         ...article,
         pictures:
-          import.meta.env.VITE_URL_DIRECTUS + "assets/" + article.pictures,
+          "http://chara-redif.vpnuser.lan/directus/uploads/" +
+          article.pictures +
+          ".jpg",
       };
     });
   };
 
-  const getArticlesByCategory = async () => {
-    for (let i = 0; i < continents.length; i++) {
-      await getArticlesByContinent(continents[i]);
-    }
-  };
 
   onMount(() => {
-    getArticlesByCategory();
+    getArticlesByContinent();
   });
 </script>
 
 <section class="category-articles">
   <h1>Articles de la catégorie</h1>
-
-  {#each continents as continent}
-    <div class="articles-continent">
-      <h2>{continent}</h2>
-      {#if articlesByContinent[continent] && articlesByContinent[continent].length > 0}
-        {#each articlesByContinent[continent] as article}
-          <div class="article-card" use:link={`/article/${article.id}`}>
-            <h3>{article.title}</h3>
-            <img src={article.pictures} alt={article.title} />
-            <div class="author-date">
-              <p>
-                Par: <b>{article.author}</b>
-                Le: <b><time datetime={article.created_at}>{new Date(article.created_at).toLocaleDateString('fr-FR')}</time></b>
-              </p>
-            </div>
-          </div>
-        {/each}
-      {:else}
-        <p>Aucun article trouvé pour ce continent.</p>
-      {/if}
-    </div>
-  {/each}
+  {#each articles as article}
+      <article class="each_articleList">
+        <h3>{article.title}</h3>
+        <div class="alignement">
+          <img class="taille" src={article.pictures} alt="illustration" />
+          <p><strong>{article.author}</strong></p>
+          <time datetime={article.created_at}
+            >{new Date(article.created_at).toLocaleDateString("fr-FR")}</time
+          >
+          <p>{article.content}</p>
+          <a use:link href="/article/{article.id}">Lire la suite</a>
+        </div>
+      </article>
+    {/each}
+  
 </section>
